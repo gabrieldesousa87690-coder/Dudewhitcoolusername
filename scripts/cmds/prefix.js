7f34e6382d7b6ab329d8a22ea2e716a2b906fd1d
gabrieldesousa87690-coder <gabrieldesousa87690@gmail.com>
2026-08-29 23:46:00 +0200

    Change bot prefix message to Portuguese

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "🌐 System prefix: %1\n🛸 Your box chat prefix: %2"
+			myPrefix: "🤍🍒 Baby, o prefixo do bot é: %1\n🌸 e do seu chat é: %2"
 		}
 	},
 
